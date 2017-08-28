448f860b7242ccb5ed411ed7fed6e5afbcf19852
wuqi <wuqi198772@gmail.com>
2017-08-28 20:42:56 +0800

    添加说明

--- src/batch.cpp
+++ src/batch.cpp
@@ -2,8 +2,10 @@
 #include "batch.hpp"
 
 int main(int argc, char **argv) {
+  //example
   batch::Glob glob("D:/Foobar2000/Equalizer Presets/S*.feq");
   while (glob) {
+    //glob.GetFileName(false)只返回文件名，不带路径 
     std::cout << glob.GetFileName() << std::endl;
     glob.Next();
   }
